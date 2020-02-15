be60425ea85440a1bfeec534bd135f345428eb13
Knastoron <bene.schlai@gmx.de>
2020-02-15 21:55:04 +0100

    Projektdateien hinzufügen.

---
+++ Skribblio/MainWindow.xaml.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Skribblio
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        List<string> _allChamps = new List<string>();
+        public string AllChamps
+        {
+            set
+            {
+                _allChamps = value.Split(',').ToList();
+                UpdatePossibles();
+                OnPropertyChanged("AllChamps");
+            }
+        }
+        private int _anzahl;
+        public int Anzahl {
+            get
+            {
+                return _anzahl;
+            }
+            set
+            {
+                if(value!= _anzahl)
+                {
+                    _anzahl = value;
+
+                    OnPropertyChanged("Anzahl");
+                    UpdatePossibles();
+                }
+            }
+        }
+
+        private string _regex;
+
+        public string Regex
+        {
+            get { return _regex; }
+            set
+            {
+                _regex = value;
+                char[] letters = value.ToCharArray();
+                regexString = ".*";
+                foreach(var s in letters)
+                {
+                    regexString += s;
+                    regexString += ".*";
+                }
+
+                OnPropertyChanged("Regex");
+                UpdatePossibles();
+            }
+        }
+
+        private string regexString = string.Empty;
+
+        public string SelectedChamps { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+        public void UpdatePossibles()
+        {
+            StringBuilder s = new StringBuilder();
+            Regex R = new Regex(regexString, RegexOptions.IgnoreCase);
+            foreach (var champ in _allChamps)
+            {
+                Console.WriteLine(champ);
+                Match m = R.Match(champ);
+                if (champ.Length == Anzahl && m.Success)
+                {
+
+                    s.Append(champ);
+                    s.Append(", ");
+                }
+                
+            }
+            SelectedChamps = s.ToString();
+
+            OnPropertyChanged("SelectedChamps");
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
